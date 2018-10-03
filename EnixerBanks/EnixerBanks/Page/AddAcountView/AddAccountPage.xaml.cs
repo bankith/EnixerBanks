bcd22667f1a27c99da8a1bb4e3fdf4cec05823b1
bank <bankstanakan@gmail.com>
2018-10-03 12:53:24 +0700

    new Test

---
+++ EnixerBanks/EnixerBanks/Page/AddAcountView/AddAccountPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using EnixerBanks.ModelApp;
+
+namespace EnixerBanks.AddAcountView
+{
+    public partial class AddAccountPage : ContentPage
+    {
+        VM.AccountTypeVM vm;
+        //public string AccountTypeText { get { return AccountTypeLabel.Text; } set { AccountTypeLabel.Text = value; } }
+        AddAccountTypePage addAccountTypeP;
+
+        public AddAccountPage()
+        {
+            InitializeComponent();
+
+            SetBinding();
+        }
+
+        private void SetBinding()
+        {
+            addAccountTypeP = new AddAccountTypePage();
+            vm = new VM.AccountTypeVM(addAccountTypeP);
+
+            addAccountTypeP.BindingContext = vm;
+            this.BindingContext = vm;
+        }
+
+        void SelectAccount_Tapped(object sender, System.EventArgs e)
+        {
+            Navigation.PushModalAsync( addAccountTypeP);
+        }
+
+        async void Next_Clicked(object sender, System.EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (btn.Text == "NEXT")
+            {
+                Loading.Start(Loader, LoaderBackground);
+
+                AccountInfo account = await Services.GetAccounts(vm.accountNO,vm.serialNO);
+
+                if( account == null){
+                    await DisplayAlert("Error", "Can't Account NO", "OK");
+                }
+
+                vm.resultVisibility = true;
+                vm.NextText = "CONFIRM";
+
+                vm.resultAccountName = account.FirstName + " " + account.LastName;
+                vm.resultAccountNumber = account.AccountNumber;
+
+                Loading.Stop(Loader, LoaderBackground);
+
+
+            }
+
+            if ( btn.Text == "CONFIRM" )
+            {
+                await Navigation.PushAsync(new OTPPage(vm.accountNO));
+                return;
+            }
+
+
+
+            
+
+        }
+    }
+}
---
+++ EnixerBanks/EnixerBanks/TransferView/NotePage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EnixerBanks.TransferView
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NotePage : ContentPage
+	{
+        private Transfer2ndPage transfer2ndPage;
+
+        public NotePage ()
+		{
+			InitializeComponent ();
+		}
+
+        public NotePage(Transfer2ndPage transfer2ndPage):this()
+        {
+            this.transfer2ndPage = transfer2ndPage;
+        }
+
+        void Save_Clicked(object sender, System.EventArgs e)
+        {
+            transfer2ndPage.Note = Note.Text;
+            Navigation.PopModalAsync();
+        }
+    }
+}
